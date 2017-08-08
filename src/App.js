f42004c511c0956878ba05f01ca0c17a99cdb429
chney111 <1326006791@qq.com>
2017-08-08 23:35:56 +0000

    hashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {BrowserRouter as Router,Route,Switch,Redirect} from 'react-router-dom';
+import {HashRouter as Router,Route,Switch,Redirect} from 'react-router-dom';
 import axios from 'axios'
 import {connect} from 'react-redux'
 import Home from './ui/pages/Home/Home.js';
